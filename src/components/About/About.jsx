53804b89d41ebdd5caeb50ea707784d309368230
Rachel-Cellulose <99023474+Rachel-Cellulose@users.noreply.github.com>
2024-06-14 16:51:10 +0100

    made changes

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -13,17 +13,22 @@ const About = () => {
           </div>
 
           <div className="about_text">
-            <h4>A fronend developer based in Lagos, Nigeria</h4>
+            <h4>A frontend developer based in Lagos, Nigeria</h4>
             <p>
               I am a passionate and dedicated software developer with expertise
-              in creating innovative solutions that enhance user experiences.<br/> My
-              journey in the tech world has equipped me with a diverse skill
-              set, allowing me to tackle complex challenges and deliver
-              high-quality projects. <br/> Whether collaborating with teams or working
-              independently, I thrive on turning ideas into reality through
-              code.
+              in creating innovative solutions that enhance user experiences.
             </p>
 
+            <p>
+              My journey in the tech world has equipped me with a diverse skill
+              set, allowing me to tackle complex challenges and deliver
+              high-quality projects.
+            </p>
+
+            <p>
+              Whether collaborating with teams or working independently, I
+              thrive on turning ideas into reality through code.
+            </p>
             <motion.div className="about_button">
               <a href="#portfolio">
                 <button className="btn btn-color"> Check Out My Work</button>
